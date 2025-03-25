346cb4d8f2b6592f85167db4d02a2e248d487bd0
Peter Tumali <ptumali@uci.edu>
2025-03-25 02:33:24 -0700

    Initial Commit: Navbar Component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react'
+import styles from './Navbar.module.css'
+import { getImageUrl } from '../../utils'
+
+export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    return (
+        <nav className={styles.navbar}>
+            {/* Navbar-left */}
+            <a href="/" className={styles.title}>
+                <img className={styles.logo} src={getImageUrl("ptumLogo.png")} alt="Logo" />
+                <span>Peter Tumali</span>
+            </a>
+
+            {/* Navbar-right */}
+            <div className={styles.menu}>
+                {/* Navbar Logo */}
+                <img 
+                    className={styles.menuBtn} 
+                    src={menuOpen ? getImageUrl("closeIcon.png") : getImageUrl("burger.png")} 
+                    alt="Menu-Button" 
+                    onClick = {() => setMenuOpen(!menuOpen)}
+                />
+
+                {/* Buttons */}
+                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                    onClick = {() => setMenuOpen(false)}
+                >
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                    <li>
+                        <a href="https://drive.google.com/file/d/1inkloZr5CJamZr65FIh80mlZSInQmFCh/view?usp=sharing"
+                            target="_blank"
+                            rel="noopener noreferrer">Resume</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
